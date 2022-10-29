9fbfd151ffc20480d4b34fcb0e86fd6229dd9ab4
yogjin <youngjin988@gmail.com>
2022-10-30 00:18:49 +0900

    feat(problem2): 암호문 해독 로직 구현
    암호문`cryptogram` 해독 로직 (`cryptogram`의 길이가 0 이상일때까지 반복)
        1. 변수 선언
            - 이전 문자
            - 중복 문자
            - 중복 문자를 저장하는 배열
        2. `cryptogram`의 0번 문자 부터 마지막 문자까지 순서대로 접근
            a. 이전 문자와 현재 문자가 같으면
                - 중복 문자에 현재 문자를 더해준 뒤 중복 문자를 저장하는 배열에 중복 문자를 push 한다
            b. 이전 문자와 현재 문자가 다르면
                - 중복 문자를 현재 문자로 할당한다
        3. 중복 문자를 저장하는 배열이
            a. 빈 경우: 해독완료 (반복문 `break`)
            b. 비어있지 않은 경우
                1. 중복 문자의 길이가 긴 순서대로 정렬
                2. replace(중복문자, “”)를 이용하여 `cryptogram` 에 중복문자를 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,36 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let decipheredCryptogram = cryptogram;
+
+  while (decipheredCryptogram.length > 0) {
+    let previousCharacter = '';
+    let duplicateCharacters = '';
+    const duplicateCharactersList = [];
+    for (let i = 0; i < decipheredCryptogram.length; i++) {
+      const currentCharacter = decipheredCryptogram[i];
+
+      if (previousCharacter === currentCharacter) {
+        duplicateCharacters += currentCharacter;
+        duplicateCharactersList.push(duplicateCharacters);
+      } else {
+        duplicateCharacters = currentCharacter;
+      }
+      previousCharacter = currentCharacter;
+    }
+
+    if (duplicateCharactersList.length === 0) {
+      break;
+    }
+
+    duplicateCharactersList.sort((a, b) => b.length - a.length);
+    duplicateCharactersList.forEach(
+      (duplicateCharacters) =>
+        (decipheredCryptogram = decipheredCryptogram.replace(
+          duplicateCharacters,
+          ''
+        ))
+    );
+  }
+  return decipheredCryptogram;
 }
 
 module.exports = problem2;
