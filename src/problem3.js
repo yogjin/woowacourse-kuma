6f7fccee57acf1b69197225184df5b150a4f9f98
yogjin <youngjin988@gmail.com>
2022-10-30 00:57:05 +0900

    feat(problem3): 369게임 로직 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCount = 0;
+
+  for (let currentNumber = 1; currentNumber <= number; currentNumber++) {
+    clapCount += [...currentNumber.toString()].filter((digit) =>
+      [3, 6, 9].includes(parseInt(digit))
+    ).length;
+  }
+
+  return clapCount;
 }
 
 module.exports = problem3;
