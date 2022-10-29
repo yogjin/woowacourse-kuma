b9cca3a31cf55dcce5e956dab63c504179dec94f
yogjin <youngjin988@gmail.com>
2022-10-29 18:09:42 +0900

    feat(problem1): 페이지 번호의 각 자리 숫자를 더해서 반환하는 함수 추가
    `page`의 타입은 [number, number]

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,10 @@
+function getSumOfEachDigitOfPage(page) {
+  let sum = 0;
+  [...page.toString()].forEach((digit) => (sum += parseInt(digit)));
+
+  return sum;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
