5a0f61c06930b332fb2ca6d487f8dac02cd47f44
yogjin <youngjin988@gmail.com>
2022-10-30 02:42:42 +0900

    feat(problem5): 화폐 변환 로직 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  const answer = [];
+  const monetaryUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  monetaryUnits.forEach((unit) => {
+    const quotient = Math.floor(money / unit);
+
+    answer.push(quotient);
+    money -= quotient * unit;
+  });
+
   return answer;
 }
 
