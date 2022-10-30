6ead8abae930fff48bb4db03e6565310751382e1
yogjin <youngjin988@gmail.com>
2022-10-30 17:43:13 +0900

    feat(problem7): 친구 추천 규칙에 따라 매긴 추천점수를 계산하는 기능 추가
    + 친구 추천 알고리즘
      + 사용자와 함께 아는 친구의 수(userFriends) = 10점
      + 사용자의 타임 라인에 방문한 횟수(visitors) = 1점

--- src/problem7.js
+++ src/problem7.js
@@ -14,6 +14,35 @@ function problem7(user, friends, visitors) {
       allUserFriendsList[id_b].push(id_a);
     }
   });
+
+  const friendRecommendationScores = {};
+  const userFriends = allUserFriendsList[user];
+
+  for (const [id, friends] of Object.entries(allUserFriendsList)) {
+    if (id === user) {
+      continue;
+    }
+
+    friends.forEach((friend) => {
+      if (userFriends.includes(friend)) {
+        if (friendRecommendationScores.hasOwnProperty(id)) {
+          friendRecommendationScores[id] += 10;
+        } else {
+          friendRecommendationScores[id] = 10;
+        }
+      }
+    });
+  }
+
+  visitors.forEach((visitorId) => {
+    if (!userFriends.includes(visitorId)) {
+      if (friendRecommendationScores.hasOwnProperty(visitorId)) {
+        friendRecommendationScores[visitorId] += 1;
+      } else {
+        friendRecommendationScores[visitorId] = 1;
+      }
+    }
+  });
 }
 
 module.exports = problem7;
