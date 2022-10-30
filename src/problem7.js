88139c18edb53d715d99b62ef26cb80f730acbc5
yogjin <youngjin988@gmail.com>
2022-10-30 17:39:54 +0900

    feat(problem7): 각 사용자들의 친구 관계를 저장하는 기능 추가
    - 사용자 아이디 user와 친구 관계를 담은 이차원 배열 friends로 부터 친구관계를 저장한다.
    - friends의 item([A, B])을 돌면서 각 사용자들의 친구 관계를 저장한다. e.q. friends = [[A, B], [B, C]] → 친구관계 = { A: [B], B: [A, C], C: [B]}

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,19 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const allUserFriendsList = {};
+
+  friends.forEach((friend) => {
+    const [id_a, id_b] = friend;
+    if (!allUserFriendsList.hasOwnProperty(id_a)) {
+      allUserFriendsList[id_a] = [id_b];
+    } else {
+      allUserFriendsList[id_a].push(id_b);
+    }
+    if (!allUserFriendsList.hasOwnProperty(id_b)) {
+      allUserFriendsList[id_b] = [id_a];
+    } else {
+      allUserFriendsList[id_b].push(id_a);
+    }
+  });
 }
 
 module.exports = problem7;
