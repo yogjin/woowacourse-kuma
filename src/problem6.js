d877e137adf3caf4457ecabe0757c81229d8931c
yogjin <youngjin988@gmail.com>
2022-10-30 15:20:36 +0900

    feat(problem6): 크루원들의 닉네임을 두글자씩 나누어 개수를 세는 기능 추가
    + forms의 각 item들의 닉네임에서 두글자씩 객체(nicknamePiecesCounts)에 저장하여 카운팅을 한다.
    + 두글자들의 중복을 없애서 저장한다. e.q. “제이제이엠” → [“제이”, “이제” , “제이”, “이엠”] → [“제이”, “이제”, “이엠”] → nicknamePiecesCounts { “제이”: 1, “이제”: 1, “이엠”: 1 }

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const nicknamePiecesCounts = {};
+
+  forms.forEach((form) => {
+    const nickname = form[1];
+    const nicknamePieces = new Set();
+
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const nicknamePiece = nickname.slice(i, i + 2);
+      nicknamePieces.add(nicknamePiece);
+    }
+
+    nicknamePieces.forEach((nicknamePiece) => {
+      if (!nicknamePiecesCounts.hasOwnProperty(nicknamePiece)) {
+        nicknamePiecesCounts[nicknamePiece] = 1;
+      } else {
+        nicknamePiecesCounts[nicknamePiece] += 1;
+      }
+    });
+  });
 }
 
 module.exports = problem6;
