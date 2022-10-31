62b93cda97fa3e6b2c7c7c9440feebd0f0414553
yogjin <youngjin988@gmail.com>
2022-10-31 17:27:25 +0900

    refactor(problem5): 로직을 함수로 분리
    - getEachMonetaryUnitCountiInOrderOfLargeAmounts: money를 화폐 액수가 큰 순서대로 나눈 몫을 담은 배열을 return

--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,19 @@
-function problem5(money) {
-  const answer = [];
+function getEachMonetaryUnitCountiInOrderOfLargeAmounts(money) {
   const monetaryUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const monetaryUnitCounts = [];
 
   monetaryUnits.forEach((unit) => {
     const quotient = Math.floor(money / unit);
 
-    answer.push(quotient);
+    monetaryUnitCounts.push(quotient);
     money -= quotient * unit;
   });
+
+  return monetaryUnitCounts;
+}
+
+function problem5(money) {
+  const answer = getEachMonetaryUnitCountiInOrderOfLargeAmounts(money);
 
   return answer;
 }
