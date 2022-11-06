50991d76db86de9f85bd28356b387f100d9fd838
yogjin <youngjin988@gmail.com>
2022-11-06 15:13:31 +0900

    feat(message): input request

--- src/message.js
+++ src/message.js
@@ -4,4 +4,10 @@ const gameStartMessage = () => {
   Console.print('숫자 야구 게임을 시작합니다.');
 };
 
-module.exports = { gameStartMessage };
+const numberInputRequestMessage = () => {
+  Console.readLine('숫자를 입력해주세요 :', (userInputNumber) => {
+    resultMessage(userInputNumber);
+  });
+};
+
+module.exports = { gameStartMessage, numberInputRequestMessage };
