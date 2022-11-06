e6338ff4ae2fd716051c9958dab38161152baaec
yogjin <youngjin988@gmail.com>
2022-11-06 14:58:34 +0900

    feat(logic): get ball and strike count

--- src/logic.js
+++ src/logic.js
@@ -4,4 +4,19 @@ const getComputerNumber = () => {
   return Random.pickUniqueNumbersInRange(1, 9, 3);
 };
 
-module.exports = { getComputerNumber };
+const getBallAndStrikeCounts = (computerNumber, userInputNumber) => {
+  let ballCount = 0;
+  let strikeCount = 0;
+
+  userInputNumber.forEach((digit, index) => {
+    if (digit === computerNumber[index]) {
+      strikeCount += 1;
+    } else if (computerNumber.includes(digit)) {
+      ballCount += 1;
+    }
+  });
+
+  return [ballCount, strikeCount];
+};
+
+module.exports = { getComputerNumber, getBallAndStrikeCounts };
