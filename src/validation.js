f00a0f2ab995a9317ce24ed49c2fb40436007e39
yogjin <youngjin988@gmail.com>
2022-11-06 15:44:27 +0900

    feat(validation): whether user number is three digit number

--- src/validation.js
+++ src/validation.js
@@ -2,4 +2,8 @@ const isAllDigitbetweenOneAndNine = (userInputNumber) => {
   return userInputNumber.every((digit) => digit >= 1 && digit <= 9);
 };
 
-module.exports = { isAllDigitbetweenOneAndNine };
+const isThreeDigitNumber = (userInputNumber) => {
+  return userInputNumber.length === 3;
+};
+
+module.exports = { isAllDigitbetweenOneAndNine, isThreeDigitNumber };
