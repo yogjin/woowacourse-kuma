7d12d484b44509cfbc950c479db5da1f4176c485
yogjin <youngjin988@gmail.com>
2022-11-06 15:17:08 +0900

    feat(message): result of user input number

--- src/message.js
+++ src/message.js
@@ -10,4 +10,21 @@ const numberInputRequestMessage = () => {
   });
 };
 
-module.exports = { gameStartMessage, numberInputRequestMessage };
+const resultMessage = (ballAndStrikeCounts) => {
+  const [ballCounts, strikeCounts] = ballAndStrikeCounts;
+
+  if (ballCounts > 0 && strikeCounts > 0) {
+    Console.print(`${ballCounts}볼 ${strikeCounts}스트라이크`);
+  } else if (ballCounts > 0 && strikeCounts === 0) {
+    Console.print(`${ballCounts}볼`);
+  } else if (strikeCounts > 0 && ballCounts === 0) {
+    Console.print(`${ballCounts}스트라이크`);
+  } else if (strikeCounts === 0 && ballCounts === 0) {
+    Console.print(`낫싱`);
+  } else if (strikeCounts === 3) {
+    Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+    Console.print(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`);
+  }
+};
+
+module.exports = { gameStartMessage, numberInputRequestMessage, resultMessage };
