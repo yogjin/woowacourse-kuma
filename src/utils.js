58649aab56cf7cc3204032d6df0fecf67de6cb83
yogjin <youngjin988@gmail.com>
2022-11-07 23:02:08 +0900

    feat(util): user input's type coercion as number

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
 const getUserInputNumberFromUserInput = (userInput) => {
   return Array.from(userInput, (digit) => parseInt(digit, 10));
 };
-module.exports = { getUserInputNumberFromUserInput };
+
+const getUserInputDigitFromUserInput = (userInputDigit) => {
+  return parseInt(userInputDigit, 10);
+};
+
+module.exports = { getUserInputNumberFromUserInput, getUserInputDigitFromUserInput };
