9ceb8c34fe3794e54aea7fcaab82acf278cb1443
yogjin <youngjin988@gmail.com>
2022-11-11 00:07:03 +0900

    feat: 로또 구입 금액이 1000원 단위가 아닌 경우 예외 처리

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ const Utils = require('./Utils');
 
 class App {
   play() {
-    Utils.readLine(`구입금액을 입력해 주세요.`, (amount) => {});
+    Utils.readLine(`구입금액을 입력해 주세요.`, (amount) => {
+      if (amount % 1000 !== 0) throw new Error('[ERROR] 1,000원 단위로 입력해주세요.');
+    });
   }
 }
 
