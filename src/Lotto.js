9eebbb09928e39d151080885f625e47ee6e611cd
yogjin <youngjin988@gmail.com>
2022-11-12 17:31:26 +0900

    feat: 당첨번호 입력에 중복된 숫자가 있는 경우 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,8 @@ class Lotto {
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    } else if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있으면 안됩니다.');
     }
   }
 
