ad4ad6abda89bf1e3fe5ebf05d4de2d6ea8a91fa
yogjin <youngjin988@gmail.com>
2022-11-12 16:16:30 +0900

    feat: 당첨 내역을 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const Company = require('./Company');
+const Lotto = require('./Lotto');
 const Utils = require('./Utils');
 
 class App {
@@ -24,13 +25,28 @@ class App {
           return lottoNumber;
         });
 
+        const lotto = new Lotto(lottoWinningNumbers);
+
         Utils.readLine(`보너스 번호를 입력해 주세요.`, (input) => {
           const bonusNumber = parseInt(input, 10);
           if (!(bonusNumber >= 1 && bonusNumber <= 45)) throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+
+          const statisitcs = company.getStatistics(lotto, bonusNumber);
+
+          Utils.print(`당첨 통계`);
+          Utils.print(`---`);
+          Utils.print(`3개 일치 (5,000원) - ${statisitcs[4]}개`);
+          Utils.print(`4개 일치 (50,000원) - ${statisitcs[3]}개`);
+          Utils.print(`5개 일치 (1,500,000원) - ${statisitcs[2]}개`);
+          Utils.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${statisitcs[1]}개`);
+          Utils.print(`6개 일치 (2,000,000,000원) - ${statisitcs[0]}개`);
         });
       });
     });
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
