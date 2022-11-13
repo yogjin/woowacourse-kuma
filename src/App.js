bb1fa48e2dc62d4bcdaf139911d5d10478f8160c
yogjin <youngjin988@gmail.com>
2022-11-13 23:26:55 +0900

    refactor: 재정의한 클래스에 맞게 play() 메소드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 const LottoMachine = require('./LottoMachine');
+const OutputView = require('./OutputView');
 const { ERROR, LOTTO_PRICE } = require('./utils/constants');
-const Lotto = require('./Lotto');
-const { readLine, print, printGeneratedLottos, printStatistic, printRateOfReturn } = require('./utils/console');
 
 class App {
   play() {
     const lottoMachine = new LottoMachine();
 
-    readLine(`구입금액을 입력해 주세요.`, (purchasedAmount) => {
+    OutputView.requestPurchaseAmount((purchasedAmount) => {
       if (purchasedAmount % LOTTO_PRICE !== 0) throw new Error(ERROR.notThousandWonUnit);
 
       lottoMachine.generateLotto(purchasedAmount);
 
       const generatedLottos = lottoMachine.getGeneratedLottos();
-      printGeneratedLottos(generatedLottos);
+      OutputView.printGeneratedLottos(generatedLottos);
 
-      readLine(`당첨 번호를 입력해 주세요.`, (input) => {
+      OutputView.requestWinningNumbers((input) => {
         const lottoWinningNumbers = input.split(',').map((number) => {
           const lottoNumber = parseInt(number, 10);
           if (!(lottoNumber >= 1 && lottoNumber <= 45)) throw new Error(ERROR.notOneToFourtyFiveRange);
@@ -23,16 +22,18 @@ class App {
           return lottoNumber;
         });
 
-        const lotto = new Lotto(lottoWinningNumbers);
+        lottoMachine.setLottoWinningNumbers(lottoWinningNumbers);
 
-        readLine(`보너스 번호를 입력해 주세요.`, (input) => {
+        OutputView.requestBonusNumber((input) => {
           const bonusNumber = parseInt(input, 10);
           if (!(bonusNumber >= 1 && bonusNumber <= 45)) throw new Error(ERROR.notOneToFourtyFiveRange);
           if (lottoWinningNumbers.includes(bonusNumber)) throw new Error(ERROR.bonusNumberExistInLottoWinnningNumbers);
 
-          const statistic = lottoMachine.getStatistic(lotto, bonusNumber);
-          printStatistic(statistic);
-          printRateOfReturn(purchasedAmount, statistic);
+          lottoMachine.setBonusNumber(bonusNumber);
+
+          const statistic = lottoMachine.getStatistic();
+          OutputView.printStatistic(statistic);
+          OutputView.printRateOfReturn(purchasedAmount, statistic);
         });
       });
     });
