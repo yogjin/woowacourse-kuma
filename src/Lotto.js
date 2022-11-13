351c082f5aba7313e2dba4859ef5d9814c898600
yogjin <youngjin988@gmail.com>
2022-11-13 23:26:19 +0900

    refactor: 역할 구분을 위해 Lotto 클래스의 일부 메소드를 LottoMachine으로 이동
    1. Lotto 클래스는 당첨번호만 가진다
    2. LottoMachine 클래스에서 Lotto의 인스턴스와 bonusNumber를 가진다.
    3. Lotto의 인스턴스와 bonusNumber는 외부에서 set 해준다.
    4. 따라서 LottoMachine 내부에서 통계를 구할 수 있다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,4 @@
-const { Random } = require('@woowacourse/mission-utils');
-const { getAscending } = require('./utils/common');
-const { RESULT, ERROR } = require('./utils/constants');
+const { ERROR } = require('./utils/constants');
 
 class Lotto {
   #numbers;
@@ -18,33 +16,8 @@ class Lotto {
     }
   }
 
-  static generate() {
-    let numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
-    numbers = getAscending(numbers);
-
-    return numbers;
-  }
-
-  getResult(userNumbers, bonusNumber) {
-    let matchCount = 0;
-
-    userNumbers.forEach((userNumber) => {
-      if (this.#numbers.includes(userNumber)) matchCount += 1;
-    });
-
-    switch (matchCount) {
-      case 6:
-        return RESULT.FIRST;
-      case 5:
-        if (this.#numbers.includes(bonusNumber)) return RESULT.SECOND;
-        return RESULT.THIRD;
-      case 4:
-        return RESULT.FOURTH;
-      case 3:
-        return RESULT.FIFTH;
-      default:
-        return RESULT.LAST;
-    }
+  getWinningNumbers() {
+    return this.#numbers;
   }
 }
 
--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -1,16 +1,35 @@
-const { RESULT } = require('./utils/constants');
+const { RESULT, LOTTO_PRICE } = require('./utils/constants');
 const Lotto = require('./Lotto');
+const { getAscending } = require('./utils/common');
+const { Random } = require('@woowacourse/mission-utils');
 
 class LottoMachine {
   generatedLottos = [];
+  lotto;
+  bonusNumber;
 
   constructor() {}
 
-  generateLotto(amount) {
-    const number = amount / 1000;
+  setLottoWinningNumbers(lottoWinningNumbers) {
+    this.lotto = new Lotto(lottoWinningNumbers);
+  }
+
+  setBonusNumber(bonusNumber) {
+    this.bonusNumber = bonusNumber;
+  }
+
+  #generate() {
+    let numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    numbers = getAscending(numbers);
+
+    return numbers;
+  }
+
+  generateLotto(purchasedAmount) {
+    const number = purchasedAmount / LOTTO_PRICE;
 
     for (let i = 0; i < number; i++) {
-      const lotto = Lotto.generate();
+      const lotto = this.#generate();
       this.generatedLottos.push(lotto);
     }
   }
@@ -23,7 +42,29 @@ class LottoMachine {
     return this.generatedLottos.length;
   }
 
-  getStatistic(lotto, bonusNumber) {
+  #getResult(generatedLotto) {
+    let matchCount = 0;
+
+    generatedLotto.forEach((number) => {
+      if (this.lotto.getWinningNumbers().includes(number)) matchCount += 1;
+    });
+
+    switch (matchCount) {
+      case 6:
+        return RESULT.FIRST;
+      case 5:
+        if (generatedLotto.includes(this.bonusNumber)) return RESULT.SECOND;
+        return RESULT.THIRD;
+      case 4:
+        return RESULT.FOURTH;
+      case 3:
+        return RESULT.FIFTH;
+      default:
+        return RESULT.LAST;
+    }
+  }
+
+  getStatistic() {
     const statistic = {
       FIRST: 0,
       SECOND: 0,
@@ -33,7 +74,7 @@ class LottoMachine {
     };
 
     for (const generatedLotto of this.generatedLottos) {
-      const result = lotto.getResult(generatedLotto, bonusNumber);
+      const result = this.#getResult(generatedLotto);
 
       switch (result) {
         case RESULT.FIRST:
