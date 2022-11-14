126d52f3aa554009e707393dfbc7c9ce2c3b30d6
yogjin <youngjin988@gmail.com>
2022-11-15 01:28:28 +0900

    test: 보너스 번호 예외처리 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -97,6 +97,22 @@ describe('LottoMachine 클래스 테스트', () => {
       });
     });
   });
+
+  describe('setBonusNumber(bonusNumber)', () => {
+    test('보너스 번호가 1~45 사이의 숫자가 아닌 경우 예외가 발생한다.', () => {
+      expect(() => {
+        lottoMachine.setBonusNumber(0);
+      }).toThrow('[ERROR]');
+    });
+
+    test('보너스 번호가 당첨 번호에 존재하는 숫자인 경우 예외가 발생한다.', () => {
+      lottoMachine.setLottoWinningNumbers([1, 2, 3, 4, 5, 6]);
+
+      expect(() => {
+        lottoMachine.setBonusNumber(1);
+      }).toThrow('[ERROR]');
+    });
+  });
 });
 
 describe('common 유틸 테스트', () => {
