48bfecf5f199fa4a06ef4b90f3f8d05cf964c773
yogjin <youngjin988@gmail.com>
2022-11-18 15:05:38 +0900

    feat: 다리 건너기 결과를 출력하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -8,7 +8,30 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(history) {
+    // { position: this.#next, upOrDown, isSuccess }
+    let upPrint = '[';
+    let downPrint = '[';
+    history.forEach((item) => {
+      if (item.position > 0) {
+        upPrint += `|`;
+        downPrint += `|`;
+      }
+      const char = item.isSuccess ? 'O' : 'X';
+      if (item.upOrDown === 'U') {
+        upPrint += ` ${char} `;
+        downPrint += `   `;
+      } else {
+        upPrint += `   `;
+        downPrint += ` ${char} `;
+      }
+    });
+    upPrint += ']';
+    downPrint += ']';
+
+    Console.print(upPrint);
+    Console.print(downPrint);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
