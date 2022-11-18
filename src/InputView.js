baf0d870e7460b26f84954b7b73528e9099d462f
yogjin <youngjin988@gmail.com>
2022-11-18 11:01:39 +0900

    feat: 라운드마다 플레이어가 이동할 칸을 입력 받는 기능 추가
    (위: U, 아래: D)

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,13 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const { Console } = require('@woowacourse/mission-utils');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+//  제공된 InputView 객체를 활용해 구현해야 한다.
+//  InputView의 파일 경로는 변경할 수 있다.
+//  InputView의 메서드의 인자는 변경할 수 있다.
+//  사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
@@ -17,7 +24,9 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(`이동할 칸을 선택해주세요. (위: U, 아래: D)`, (input) => {});
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
