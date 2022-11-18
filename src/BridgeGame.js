c704ab8db8ccbf4e7a3374648d80418afbe8aa3a
yogjin <youngjin988@gmail.com>
2022-11-18 17:37:51 +0900

    feat: 'R'을 입력받으면 게임을 재시작하는 기능 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -59,7 +59,11 @@ class BridgeGame {
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.#next = 0;
+    this.#history = [];
+    this.process3();
+  }
 
   quit() {}
 
--- src/InputView.js
+++ src/InputView.js
@@ -40,6 +40,7 @@ const InputView = {
       `게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)`,
       (restartOrQuit) => {
         if (restartOrQuit === 'R') {
+          retry();
         } else {
         }
       },
