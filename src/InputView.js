0daa5920c43620232d68608e60b45e53dfadd091
yogjin <youngjin988@gmail.com>
2022-11-18 20:12:30 +0900

    feat: 재시작/종료 여부를 입력받을 때 'R'와 'Q'중 하나의 문자가 아닌 경우 예외처리

--- src/InputView.js
+++ src/InputView.js
@@ -39,16 +39,14 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand(retry, quit) {
-    Console.readLine(
-      `게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)`,
-      (restartOrQuit) => {
-        if (restartOrQuit === 'R') {
-          retry();
-        } else {
-          quit();
-        }
-      },
-    );
+    Console.readLine(`게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)`, (restartOrQuit) => {
+      if (!['R', 'Q'].includes(restartOrQuit)) throw new Error(`[ERROR] 'R' 또는 'Q'를 입력해주세요.`);
+      if (restartOrQuit === 'R') {
+        retry();
+      } else {
+        quit();
+      }
+    });
   },
 };
 
