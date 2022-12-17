c9435363392563e10ef0015bc6ef5d8c0d8f858a
yogjin <youngjin988@gmail.com>
2022-12-17 13:38:52 +0900

    feat: 못 먹는 메뉴를 입력받는 기능 추가
    각 코치 별로 못먹는 메뉴를 입력받아서 저장한다.

--- src/domain/MenuProcessor.js
+++ src/domain/MenuProcessor.js
@@ -1,8 +1,10 @@
-const { readCoachNames } = require('../ui/InputView');
+const { readCoachNames, readUnlikeMenu } = require('../ui/InputView');
 const { printStartMenuRecommadationMessage } = require('../ui/OutputView');
 
 class MenuProcessor {
   #coachs = [];
+  #coachIndex = 0;
+  #unlikeMenu = {};
 
   start() {
     this.#startProcess();
@@ -16,10 +18,22 @@ class MenuProcessor {
   // 코치 이름 받기
   #setCoachNames(input) {
     this.#coachs = input.split(',').map((name) => name.trim());
+    this.#setUnlikeMenuProcess(this.#coachIndex);
   }
 
   #setCoachNamesProcess() {
     readCoachNames(this.#setCoachNames.bind(this));
+  }
+
+  // 못 먹는 메뉴 입력받기
+  #setUnlikeMenu(input) {
+    this.#unlikeMenu[this.#coachs[this.#coachIndex]] = input.split(',').map((name) => name.trim());
+    this.#coachIndex += 1;
+    this.#setUnlikeMenuProcess(this.#coachIndex);
+  }
+
+  #setUnlikeMenuProcess(coachIndex) {
+    readUnlikeMenu(this.#coachs[coachIndex], this.#setUnlikeMenu.bind(this));
   }
 }
 
--- src/ui/InputView.js
+++ src/ui/InputView.js
@@ -8,6 +8,10 @@ const InputView = {
   readCoachNames(setCoachNames) {
     readLine('코치의 이름을 입력해 주세요. (, 로 구분)', setCoachNames);
   },
+
+  readUnlikeMenu(name, setUnlikeMenu) {
+    readLine(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.`, setUnlikeMenu);
+  },
 };
 
 module.exports = InputView;
