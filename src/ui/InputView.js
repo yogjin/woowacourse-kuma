09a2d66ff82ee03e08df41407c1218f9a9b925ea
yogjin <youngjin988@gmail.com>
2022-12-17 16:51:22 +0900

    chore: 콘솔 출력 띄어쓰기 추가

--- src/ui/InputView.js
+++ src/ui/InputView.js
@@ -6,11 +6,11 @@ const readLine = (text, callback) => {
 
 const InputView = {
   readCoachNames(setCoachNames) {
-    readLine('코치의 이름을 입력해 주세요. (, 로 구분)', setCoachNames);
+    readLine('코치의 이름을 입력해 주세요. (, 로 구분)\n', setCoachNames);
   },
 
   readUnlikeMenu(name, setUnlikeMenu) {
-    readLine(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.`, setUnlikeMenu);
+    readLine(`\n${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`, setUnlikeMenu);
   },
 };
 
--- src/ui/OutputView.js
+++ src/ui/OutputView.js
@@ -29,9 +29,11 @@ const OutputView = {
 
   printStartMenuRecommadationMessage() {
     print(`점심 메뉴 추천을 시작합니다.`);
+    printBlank();
   },
 
   printMenuRecommendationResult(categories, recommendationResult) {
+    printBlank();
     print(`메뉴 추천 결과입니다.`);
     printBlank();
     print('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
